918f3d5e7690d432366f0f2e4f14126ba254267a
kindly-framing <saa224@njit.edu>
2023-01-05 13:34:13 -0500

    added a simple hashing function for pairs

--- includes/helpers.h
+++ includes/helpers.h
@@ -36,4 +36,16 @@ template <typename T> void display(const std::vector<T> &vec)
     std::cout << '\n';
 }
 
+// Usuable simple hash for pairs.
+struct Pair_Hash
+{
+    template <class T1, class T2>
+    std::size_t operator()(const std::pair<T1, T2> &p) const
+    {
+        auto h1 = std::hash<T1>{}(p.first);
+        auto h2 = std::hash<T2>{}(p.second);
+        return h1 ^ h2;
+    }
+};
+
 #endif
