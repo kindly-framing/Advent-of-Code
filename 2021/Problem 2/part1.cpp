af82147a2b9df2f4a336b3e6901cff27af2f5e52
kindly-framing <saa224@njit.edu>
2023-01-06 02:49:22 -0500

    Refactored and added comments

--- 2021/Problem 2/part1.cpp
+++ 2021/Problem 2/part1.cpp
@@ -1,7 +1,10 @@
-#include <fstream>
-#include <iostream>
-#include <string>
+#include "helpers.h"
 
+/**
+ * @brief A Submarine object that takes a series of commands to change it's
+ * horizontal position and depth.
+ *
+ */
 struct Submarine
 {
     int pos;
@@ -9,37 +12,68 @@ struct Submarine
 
     Submarine() : pos{}, depth{} {}
 
+    /**
+     * @brief Executes the command given after checking if the command can be
+     * executed.
+     *
+     * @throw std::invalid_argument Command must be in the correct format.
+     * (direction X) with X being an integer.
+     * @param command An executable submarine command.
+     */
     void execute(const std::string &command)
     {
-        int val = std::stoi(command.substr(command.find(' ') + 1));
+        if (!is_executable(command))
+        {
+            throw std::invalid_argument("Not a submarine command: " + command);
+        }
+
+        int units;
+        sscanf(command.c_str(), "%*s %d", &units);
         switch (command[0])
         {
         case 'f':
-            pos += val;
+            pos += units;
             break;
         case 'u':
-            depth -= val;
+            depth -= units;
             break;
         case 'd':
-            depth += val;
+            depth += units;
             break;
-        default:
-            throw std::invalid_argument("Not a valid command: " + command);
         }
+    }
+
+    /**
+     * @brief Checks if the given string is an executable Submarine command.
+     *
+     * @param str A string.
+     * @return If the command can be executed on the submarine.
+     */
+    bool is_executable(const std::string &str)
+    {
+        char direction[8];
+        int units;
+        char terminated;
+        sscanf(str.c_str(), "%8s %d%c", direction, &units, &terminated);
+        std::string dir = direction;
+
+        bool is_direction = (dir == "forward" || dir == "down" || dir == "up");
+        bool is_positive_units = (units >= 0);
+        bool is_end_of_string = (terminated == '\0');
+        return is_direction && is_positive_units && is_end_of_string;
     }
 };
 
 int main()
 {
-    std::ifstream in("sample.txt");
+    std::vector<std::string> lines = get_lines("sample.txt");
 
     Submarine sub;
-    std::string command;
-    while (std::getline(in, command))
+    for (std::string command : lines)
     {
         sub.execute(command);
     }
-
     std::cout << sub.depth * sub.pos << '\n';
+
     return 0;
 }
--- 2021/Problem 2/part2.cpp
+++ 2021/Problem 2/part2.cpp
@@ -1,50 +1,85 @@
-// reading each command
-// apply movement to submarine
-// end multiply horizontal position and depth
-#include <fstream>
-#include <iostream>
-#include <string>
+#include "helpers.h"
 
+/**
+ * @brief A Submarine object that takes a series of commands to change it's
+ * horizontal position, depth, and aim.
+ *
+ */
 struct Submarine
 {
-    int pos;
-    int depth;
-    int aim;
+    int m_pos;
+    int m_depth;
+    int m_aim;
 
-    Submarine() : pos{}, depth{}, aim{} {}
+    Submarine() : m_pos{}, m_depth{}, m_aim{} {}
 
+    /**
+     * @brief Executes the command given after checking if the command can be
+     * executed.
+     *
+     * @throw std::invalid_argument Command must be in the correct format.
+     * (direction X) with X being an integer.
+     * @param command An executable submarine command.
+     */
     void execute(const std::string &command)
     {
-        int val = std::stoi(command.substr(command.find(' ') + 1));
+        if (!is_executable(command))
+        {
+            throw std::invalid_argument("Not a submarine command: " + command);
+        }
+
+        // for part 2, the commands have been changed to reflect the addition of
+        // the aim value.
+        int units;
+        sscanf(command.c_str(), "%*s %d", &units);
         switch (command[0])
         {
         case 'f':
-            pos += val;
-            depth += aim * val;
+            m_pos += units;
+            m_depth += m_aim * units;
             break;
         case 'u':
-            aim -= val;
+            m_aim -= units;
             break;
         case 'd':
-            aim += val;
+            m_aim += units;
             break;
-        default:
-            throw std::invalid_argument("Not a valid command: " + command);
         }
+    }
+
+    /**
+     * @brief Checks if the given string is an executable Submarine command.
+     *
+     * @param str A string.
+     * @return If the command can be executed on the submarine.
+     */
+    bool is_executable(const std::string &str)
+    {
+        // reading word, number, and last character in string.
+        char direction[8];
+        int units;
+        char last;
+        sscanf(str.c_str(), "%8s %d%c", direction, &units, &last);
+        std::string dir = direction;
+
+        // checking that characteristics of an executable command are met.
+        bool is_direction = (dir == "forward" || dir == "down" || dir == "up");
+        bool is_positive_units = (units >= 0);
+        bool is_end_of_string = (last == '\0');
+        return is_direction && is_positive_units && is_end_of_string;
     }
 };
 
 int main()
 {
-    std::ifstream in("sample.txt");
+    std::vector<std::string> lines = get_lines("sample.txt");
 
     Submarine sub;
-    std::string command;
-    while (std::getline(in, command))
+    for (std::string command : lines)
     {
         sub.execute(command);
     }
+    std::cout << sub.m_depth * sub.m_pos << '\n';
 
-    std::cout << sub.depth * sub.pos << '\n';
     return 0;
 }
